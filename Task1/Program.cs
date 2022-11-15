36b9e929b64c03a3ec4982e4d76f47b28b869865
Viktoriia Chebera <cheberavika@gmail.com>
2022-11-15 17:28:04 +0200

    changed first 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Курси Sana Commerce ");
+Console.WriteLine("Курси Sana Commerce ");
+Console.WriteLine("Курси Sana Commerce ");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
