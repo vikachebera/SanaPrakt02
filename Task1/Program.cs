53ab1ded6dc1fa8ba00aa52567b2fda2c96a0760
Viktoriia Chebera <cheberavika@gmail.com>
2022-11-15 15:22:05 +0200

    +5 copies

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
